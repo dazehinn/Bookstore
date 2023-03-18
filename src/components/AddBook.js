0a2aafab0a43b1aa360e3ad3dd67f5161e9c496c
dazehinn <dazehinn.yahoo.fr>
2023-03-18 17:21:05 +0100

    Add handler to component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,9 +1,32 @@
-const AddBook = () => (
-  <form action="">
-    <input type="text" name="title" placeholder="Title" />
-    <input type="text" name="author" placeholder="Author" />
-    <input type="submit" value="Add Book" />
-  </form>
-);
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
+const AddBook = () => {
+  const bookArray = useSelector((state) => state.book.booksArray);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    const obj = {
+      id: (bookArray.length + 1).toString(),
+      title,
+      author,
+    };
+    dispatch(addBook(obj));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleAddBook}>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+      <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
+      <input type="submit" value="Add Book" />
+    </form>
+  );
+};
 
 export default AddBook;
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,20 @@
-const Categories = () => (
-  <section className="categories">
-    <button type="button">Check status</button>
-  </section>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+
+const Categories = () => {
+  const categoriesArray = useSelector((state) => state.categorie.categoriesArray);
+  const dispatch = useDispatch();
+
+  const handleCheckStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <section className="categories">
+      <button onClick={handleCheckStatus} type="button">Check status</button>
+      {categoriesArray.length > 0 ? <p>{categoriesArray[0]}</p> : <p> </p>}
+    </section>
+  );
+};
 
 export default Categories;
