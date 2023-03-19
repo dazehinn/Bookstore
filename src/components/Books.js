e2707e28266733dec6ae603dbd984b0fcd9fc0a9
dazehinn <dazehinn.yahoo.fr>
2023-03-19 04:39:24 +0100

    Update component for styling

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,27 @@
 import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import AddBook from './AddBook';
 import Book from './Book';
+import style from './Books.module.css';
 
 const BookList = () => {
-  const bookArray = useSelector((state) => state.book.booksArray);
+  const state = useSelector((state) => state.book);
+
+  if (!state.bookArray) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
-    <section className="bookList">
-      <ul>
-        {bookArray.map((elem) => (
-          <li key={elem.title}>
-            <Book id={elem.id} title={elem.title} author={elem.author} />
-          </li>
+    <section className={style.sectionBooks}>
+      <ul className={style.ul}>
+        {Object.entries(state.bookArray).map(([key]) => (
+          <Book
+            key={uuidv4()}
+            id={key}
+            title={state.bookArray[key][0].title}
+            author={state.bookArray[key][0].author}
+            category={state.bookArray[key][0].category}
+          />
         ))}
       </ul>
       <AddBook />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
+import style from './Header.module.css';
+import user from '../assets/images/user.png';
 
 const Header = () => (
 
-  <div className="header">
-    <nav>
-      <ul>
-        <li><Link to="/"><span className="logo">Bookstore CMS</span></Link></li>
-        <li><Link to="/books">books</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
+  <div className={style.header}>
+    <nav className={style.nav}>
+      <ul className={style.ul}>
+        <div>
+          <li><Link to="/"><span className={style.bookstoreCMS}>Bookstore CMS</span></Link></li>
+        </div>
+        <li className={style.link}><Link to="/books">BOOKS</Link></li>
+        <li className={style.link}><Link to="/categories">CATEGORIES</Link></li>
+        <li className={style.liUser}><img className={style.img} src={user} alt="" /></li>
 
       </ul>
     </nav>
