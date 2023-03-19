7d439f99eae138f2e048d873fa66027e0a780be8
dazehinn <dazehinn.yahoo.fr>
2023-03-19 04:40:08 +0100

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGetBooks } from './redux/books/booksSlice';
 import './App.css';
 import Books from './components/Books';
 import Header from './components/Header';
 import Categories from './components/Categories';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.book);
+
+  useEffect(() => {
+    if (!state.bookArray) {
+      dispatch(fetchGetBooks());
+    }
+  }, [state.bookArray, dispatch]);
   return (
     <BrowserRouter>
       <Header />
